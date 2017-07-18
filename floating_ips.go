b20436c7006f85903b53ff03040290b19f0b9c85
Antonio Cabrera <antonio.cabreraglz@gmail.com>
2017-07-18 11:42:41 -0500

    Add FloatingIPRequest type

--- floating_ips.go
+++ floating_ips.go
@@ -6,3 +6,9 @@ type FloatingIP struct {
 	NextHop string `json:"next_hop"`
 	Server  Server `json:"server"`
 }
+
+type FloatingIPRequest struct {
+	IPVersion      string `json:"ip_version"`
+	Server         string `json:"server"`
+	ReversePointer string `json:"reverse_ptr"`
+}
