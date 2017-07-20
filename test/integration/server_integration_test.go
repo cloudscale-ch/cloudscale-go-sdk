9134925b284928d0d9d56231ac6935a843d8b9d4
Antonio Cabrera <antonio.cabreraglz@gmail.com>
2017-07-20 18:28:45 -0500

    Add a TestIntegrationServer_CRUD, to test the CRUD functionality
    - After creating the server, sleeps for 15s to allow for other
      operations
    - Tests the Create, Get, List and Delete functions

--- test/integration/server_integration_test.go
+++ test/integration/server_integration_test.go
@@ -2,14 +2,60 @@
 
 package integration
 
-import "testing"
+import (
+	"context"
+	"testing"
+	"time"
 
-func acceptanceTest(t *testing.T) {
+	"github.com/cloudscale-ch/cloudscale"
+)
+
+func integrationTest(t *testing.T) {
 	if testing.Short() {
 		t.Skip("skipping acceptance test")
 	}
 }
 
-func TestAcceptanceServer_Create(t *testing.T) {
-	acceptanceTest(t)
+func TestIntegrationServer_CRUD(t *testing.T) {
+	integrationTest(t)
+
+	createRequest := &cloudscale.ServerRequest{
+		Name:         "db-master",
+		Flavor:       "flex-2",
+		Image:        "debian-8",
+		VolumeSizeGB: 10,
+		SSHKeys: []string{
+			"ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBFEepRNW5hDct4AdJ8oYsb4lNP5E9XY5fnz3ZvgNCEv7m48+bhUjJXUPuamWix3zigp2lgJHC6SChI/okJ41GUY=",
+		},
+	}
+
+	expected, err := client.Servers.Create(context.Background(), createRequest)
+	if err != nil {
+		t.Fatalf("Servers.Create returned error %s\n", err)
+	}
+	time.Sleep(15 * time.Second)
+
+	server, err := client.Servers.Get(context.Background(), expected.UUID)
+	if err != nil {
+		t.Fatalf("Servers.Get returned error %s\n", err)
+	}
+
+	if uuid := server.UUID; uuid != expected.UUID {
+		t.Errorf("Server.UUID \n got=%s\nwant=%s", uuid, expected.UUID)
+	}
+
+	servers, err := client.Servers.List(context.Background())
+	if err != nil {
+		t.Fatalf("Servers.List returned error %s\n", err)
+	}
+
+	if numServers := len(servers); numServers < 0 {
+		t.Errorf("Server.List \n got=%d\nwant=%d", numServers, 1)
+	}
+
+	err = client.Servers.Delete(context.Background(), server.UUID)
+	if err != nil {
+		t.Fatalf("Servers.Get returned error %s\n", err)
+	}
+
 }
