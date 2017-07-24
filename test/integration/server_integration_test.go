8c43a64f8430064fb844c7ffde86b377d85ee021
Antonio Cabrera <antonio.cabreraglz@gmail.com>
2017-07-24 13:50:32 -0500

    Add an integration test for the Server.Update function

--- test/integration/server_integration_test.go
+++ test/integration/server_integration_test.go
@@ -43,7 +43,7 @@ func TestIntegrationServer_CRUD(t *testing.T) {
 	}
 
 	if uuid := server.UUID; uuid != expected.UUID {
-		t.Errorf("Server.UUID \n got=%s\nwant=%s", uuid, expected.UUID)
+		t.Errorf("Server.UUID got=%s\nwant=%s", uuid, expected.UUID)
 	}
 
 	servers, err := client.Servers.List(context.Background())
@@ -52,7 +52,7 @@ func TestIntegrationServer_CRUD(t *testing.T) {
 	}
 
 	if numServers := len(servers); numServers < 0 {
-		t.Errorf("Server.List \n got=%d\nwant=%d", numServers, 1)
+		t.Errorf("Server.List got=%d\nwant=%d\n", numServers, 1)
 	}
 
 	err = client.Servers.Delete(context.Background(), server.UUID)
@@ -60,6 +60,62 @@ func TestIntegrationServer_CRUD(t *testing.T) {
 		t.Fatalf("Servers.Get returned error %s\n", err)
 	}
 
+}
+
+func TestIntegrationServer_Update(t *testing.T) {
+	integrationTest(t)
+
+	createRequest := &cloudscale.ServerRequest{
+		Name:         "db-master",
+		Flavor:       "flex-2",
+		Image:        "debian-8",
+		VolumeSizeGB: 10,
+		SSHKeys: []string{
+			"ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBFEepRNW5hDct4AdJ8oYsb4lNP5E9XY5fnz3ZvgNCEv7m48+bhUjJXUPuamWix3zigp2lgJHC6SChI/okJ41GUY=",
+		},
+	}
+
+	server, err := client.Servers.Create(context.Background(), createRequest)
+	if err != nil {
+		t.Fatalf("Servers.Create returned error %s\n", err)
+	}
+
+	waitUntil(cloudscale.ServerRunning, server.UUID, t)
+
+	// Stop a server
+	err = client.Servers.Update(context.Background(), server.UUID, cloudscale.ServerStopped)
+	if err != nil {
+		t.Fatalf("Servers.Update returned error %s\n", err)
+	}
+	s := waitUntil(cloudscale.ServerStopped, server.UUID, t)
+	if status := s.Status; status != cloudscale.ServerStopped {
+		t.Errorf("Server.Update got=%s\nwant=%s\n", status, cloudscale.ServerStopped)
+	}
+
+	// Start a server
+	err = client.Servers.Update(context.Background(), server.UUID, cloudscale.ServerRunning)
+	if err != nil {
+		t.Fatalf("Servers.Update returned error %s\n", err)
+	}
+	s = waitUntil(cloudscale.ServerRunning, server.UUID, t)
+	if status := s.Status; status != cloudscale.ServerRunning {
+		t.Errorf("Server.Update got=%s\nwant=%s\n", status, cloudscale.ServerRunning)
+	}
+
+	// Reboot a server
+	err = client.Servers.Update(context.Background(), server.UUID, cloudscale.ServerRebooted)
+	if err != nil {
+		t.Fatalf("Servers.Update returned error %s\n", err)
+	}
+	s = waitUntil(cloudscale.ServerRunning, server.UUID, t)
+	if status := s.Status; status != cloudscale.ServerRunning {
+		t.Errorf("Server.Update got=%s\nwant=%s\n", status, cloudscale.ServerRunning)
+	}
+
+	err = client.Servers.Delete(context.Background(), server.UUID)
+	if err != nil {
+		t.Fatalf("Servers.Delete returned error %s\n", err)
+	}
 }
 
 func TestIntegrationServer_Actions(t *testing.T) {
@@ -82,19 +138,35 @@ func TestIntegrationServer_Actions(t *testing.T) {
 
 	waitUntil("running", server.UUID, t)
 
+	// Stop a server
 	err = client.Servers.Stop(context.Background(), server.UUID)
 	if err != nil {
 		t.Fatalf("Servers.Stop returned error %s\n", err)
 	}
+	s := waitUntil("stopped", server.UUID, t)
+	if status := s.Status; status != cloudscale.ServerStopped {
+		t.Errorf("Server.Stop got=%s\nwant=%s\n", status, cloudscale.ServerStopped)
+	}
 
-	waitUntil("stopped", server.UUID, t)
-
+	// Start a server
 	err = client.Servers.Start(context.Background(), server.UUID)
 	if err != nil {
 		t.Fatalf("Servers.Start returned error %s\n", err)
 	}
+	s = waitUntil("running", server.UUID, t)
+	if status := s.Status; status != cloudscale.ServerRunning {
+		t.Errorf("Server.Start got=%s\nwant=%s\n", status, cloudscale.ServerRunning)
+	}
 
-	waitUntil("running", server.UUID, t)
+	// reboot server
+	err = client.Servers.Reboot(context.Background(), server.UUID)
+	if err != nil {
+		t.Fatalf("Servers.Reboot returned error %s\n", err)
+	}
+	s = waitUntil("running", server.UUID, t)
+	if status := s.Status; status != cloudscale.ServerRunning {
+		t.Errorf("Server.Reboot got=%s\nwant=%s\n", status, cloudscale.ServerRunning)
+	}
 
 	err = client.Servers.Delete(context.Background(), server.UUID)
 	if err != nil {
