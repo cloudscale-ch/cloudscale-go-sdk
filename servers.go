3cd44c24f87e1f154d5c2688cb7f2e5c8ab42aca
Antonio Cabrera <antonio.cabreraglz@gmail.com>
2017-08-11 11:36:38 -0500

    Use a stub instead of the whole Server object

--- servers.go
+++ servers.go
@@ -13,17 +13,22 @@ const ServerStopped = "stopped"
 const ServerRebooted = "rebooted"
 
 type Server struct {
-	HREF            string      `json:"href"`
-	UUID            string      `json:"uuid"`
-	Name            string      `json:"name"`
-	Status          string      `json:"status"`
-	Flavor          Flavor      `json:"flavor"`
-	Image           Image       `json:"image"`
-	Volumes         []Volume    `json:"volumes"`
-	Interfaces      []Interface `json:"interfaces"`
-	SSHFingerprints []string    `json:"ssh_fingerprints"`
-	SSHHostKeys     []string    `json:"ssh_host_keys"`
-	AntiAfinityWith []Server    `json:"anti_affinity_with"`
+	HREF            string       `json:"href"`
+	UUID            string       `json:"uuid"`
+	Name            string       `json:"name"`
+	Status          string       `json:"status"`
+	Flavor          Flavor       `json:"flavor"`
+	Image           Image        `json:"image"`
+	Volumes         []Volume     `json:"volumes"`
+	Interfaces      []Interface  `json:"interfaces"`
+	SSHFingerprints []string     `json:"ssh_fingerprints"`
+	SSHHostKeys     []string     `json:"ssh_host_keys"`
+	AntiAfinityWith []ServerStub `json:"anti_affinity_with"`
+}
+
+type ServerStub struct {
+	HREF string `json:"href"`
+	UUID string `json:"uuid"`
 }
 
 type Flavor struct {
