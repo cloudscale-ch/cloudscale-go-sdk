be6965040d49384a0a9d3c9ee4d0b62f34efc1a1
Alain Kaeslin <alain@cloudscale.ch>
2023-01-25 09:05:24 +0100

    Add HTTP to health monitor.

--- test/integration/load_balancer_health_monitors_integration_test.go
+++ test/integration/load_balancer_health_monitors_integration_test.go
@@ -143,3 +143,90 @@ func TestIntegrationLoadBalancerHealthMonitor_Update(t *testing.T) {
 		t.Fatalf("LoadBalancers.Delete returned error %s\n", err)
 	}
 }
+
+func TestIntegrationLoadBalancerHealthMonitor_HTTP_Update(t *testing.T) {
+	integrationTest(t)
+
+	lb, err := createLoadBalancer()
+	if err != nil {
+		t.Fatalf("LoadBalancers.Create returned error %s\n", err)
+	}
+
+	waitUntilLB("running", lb.UUID, t)
+
+	pool, err := createPoolOnLB(lb)
+	if err != nil {
+		t.Fatalf("LoadBalancerPools.Create returned error %s\n", err)
+	}
+
+	createLoadBalancerHealthMonitorRequest := &cloudscale.LoadBalancerHealthMonitorRequest{
+		Pool:       pool.UUID,
+		Delay:      20,
+		Timeout:    15,
+		Type:       "http",
+		MaxRetries: 10,
+	}
+
+	healthMonitor, err := client.LoadBalancerHealthMonitors.Create(context.Background(), createLoadBalancerHealthMonitorRequest)
+	if err != nil {
+		t.Fatalf("LoadBalancerHealthMonitors.Create returned error %s\n", err)
+	}
+
+	http := healthMonitor.HTTP
+	expectedHTTP := cloudscale.LoadBalancerHealthMonitorHTTPOption{
+		ExpectedCodes: []string{"200"},
+		Method:        "GET",
+		UrlPath:       "/",
+		Version:       "1.0",
+		DomainName:    nil,
+	}
+
+	if !reflect.DeepEqual(http, expectedHTTP) {
+		t.Errorf("healthMonitor.HTTP \n got=%#v\nwant=%#v", http, expectedHTTP)
+	}
+
+	//domainName := "example.com"
+	updateRequest := &cloudscale.LoadBalancerHealthMonitorRequest{
+		HTTP: cloudscale.LoadBalancerHealthMonitorHTTPRequest{
+			ExpectedCodes: []string{"201", "200"},
+		},
+	}
+
+	uuid := healthMonitor.UUID
+	err = client.LoadBalancerHealthMonitors.Update(context.Background(), uuid, updateRequest)
+	if err != nil {
+		t.Fatalf("LoadBalancerHealthMonitors.Update returned error %s\n", err)
+	}
+
+	updated, err := client.LoadBalancerHealthMonitors.Get(context.Background(), uuid)
+	if err != nil {
+		t.Fatalf("LoadBalancerHealthMonitors.Get returned error %s\n", err)
+	}
+
+	expectedUpdatedHTTP := cloudscale.LoadBalancerHealthMonitorHTTPOption{
+		ExpectedCodes: []string{"201", "200"},
+		Method:        "GET",
+		UrlPath:       "/",
+		Version:       "1.0",
+		DomainName:    nil,
+	}
+	updatedHttp := updated.HTTP
+	if !reflect.DeepEqual(updatedHttp, expectedUpdatedHTTP) {
+		t.Errorf("updated.HTTP \n got=%#v\nwant=%#v", updatedHttp, expectedUpdatedHTTP)
+	}
+
+	err = client.LoadBalancerHealthMonitors.Delete(context.Background(), updated.UUID)
+	if err != nil {
+		t.Fatalf("LoadBalancerHealthMonitors.Delete returned error %s\n", err)
+	}
+
+	err = client.LoadBalancerPools.Delete(context.Background(), pool.UUID)
+	if err != nil {
+		t.Fatalf("LoadBalancerPools.Delete returned error %s\n", err)
+	}
+
+	err = client.LoadBalancers.Delete(context.Background(), lb.UUID)
+	if err != nil {
+		t.Fatalf("LoadBalancers.Delete returned error %s\n", err)
+	}
+}
